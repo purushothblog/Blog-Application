e09e19edfb4290b0cbfd2e5105c3b4b16263c10f
pruzoth <pruzoth@gmail.com>
2014-08-19 13:04:02 +0530

    Add Validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
     belongs_to :post
+
+
+    validates_presence_of :post_id
+    validates_presence_of :body
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
+
+    validates_presence_of :title
+    validates_presence_of :body
 end

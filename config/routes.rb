589ad6c9b1a79f8070d253c9a602e2d61a9ef242
pruzoth <pruzoth@gmail.com>
2014-08-19 14:07:08 +0530

    DB change to mysql

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
-
-  resources :posts do
-    resources :comments
-  end
-
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
